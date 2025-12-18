8a0eb46c8254574318fbacf8d322cf2e4b89bc97
Mohamed Maher <mohamedmaher.developer@gmail.com>
2025-12-18 19:27:32 +0200

    os

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:brisc_detector/home/views/home_view.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-void main() =>
-    runApp(MaterialApp(home: HomeView(), debugShowCheckedModeBanner: false));
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
+  runApp(MaterialApp(home: HomeView(), debugShowCheckedModeBanner: false));
+}
